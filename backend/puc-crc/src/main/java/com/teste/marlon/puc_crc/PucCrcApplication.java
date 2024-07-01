15749472a2b698215e2b1152314d332d97c46453
marlon-bento <marlon_lion@hotmail.com>
2024-07-01 18:45:37 -0300

    primeiro commit, começando back end

---
+++ backend/puc-crc/src/main/java/com/teste/marlon/puc_crc/PucCrcApplication.java
@@ -0,0 +1,13 @@
+package com.teste.marlon.puc_crc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PucCrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PucCrcApplication.class, args);
+	}
+
+}
---
+++ backend/puc-crc/src/test/java/com/teste/marlon/puc_crc/PucCrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.marlon.puc_crc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PucCrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
